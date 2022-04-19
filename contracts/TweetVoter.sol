14701f16e6a751dc5688907fb555463a6e3d7b94
Martijn <martijncvv@gmail.com>
2022-04-18 21:12:36 -0600

    ci: setup of main smartcontracts

--- contracts/TweetVoter.sol
+++ contracts/TweetVoter.sol
@@ -35,7 +35,171 @@ contract TVToken is ERC20, AccessControl {
 
 
 
-// interface AggregatorV3Interface {
+
+interface ITVToken {
+    function mint(address, uint256) external;
+    function burn(address, uint256) external;
+    function balanceOf(address) external view returns(uint);
+}
+
+// contract TweetVoter is Ownable, AggregatorV3Interface {
+contract TweetVoter  is Ownable {
+    event SubmitTweet(address indexed user, uint256 indexed tweetId, uint256 timestamp);
+    event LikedTweet(uint256 tweetId, uint256 likes, uint256 timestamp);
+    event Log(string func, address sender, uint value, bytes data);
+
+    using SafeMath for uint256;
+
+    ITVToken tvTokenContract;
+    AggregatorV3Interface internal priceFeed;
+    address private _owner;
+
+    uint256 totalTweetsCount = 0;
+
+    uint256 platformFee = 5; // %
+
+    uint256 tweetFeeToken = 5; // 2 dollar
+    uint256 tweetFeeEth = 0.00065 ether;
+    uint256 likeFeeToken = 1; // %
+    uint256 likeFeeEth = 0.00015 ether; // %
+
+    
+    struct Tweet {
+        uint256 tweetId;
+        string tweetUrl;
+        address feeReceiver;
+        address tweetOwner;
+        uint256 likes;
+        uint256 timestamp;
+    }
+    
+    mapping(address => uint) public accountTweetsAmount;
+
+    mapping(address => uint[]) public accountToTweetIds;
+    mapping(uint => Tweet) public tweetIdToTweet;
+
+    constructor(address _tvTokenContract) {
+        console.log("Constructor: Tweet Voter deployed");
+
+        tvTokenContract = ITVToken(_tvTokenContract);
+     
+    }
+
+    fallback() external payable {
+        emit Log("fallback", msg.sender, msg.value, msg.data);
+    }
+
+    /// @notice Receives Ether if no tx data was added
+    receive() external payable {
+        emit Log("receive", msg.sender, msg.value, "");
+    }
+
+    function withdraw(address payable to, uint amount) external onlyOwner {
+        (bool sent, ) = to.call{value: amount}("");
+        require(sent, "Failed to send ETH");
+    }
+
+
+    function submitTweet(
+        string memory _tweetUrl,
+        address _feeReceiver
+    ) external payable {
+        require(bytes(_tweetUrl).length > 0,"TweetVoter: tweetUrl_INVALID");
+        require(_feeReceiver != address(0),"TweetVoter: feeReceiver_ZERO_ADDR");
+
+        uint accountTokenBalance = tvTokenContract.balanceOf(msg.sender);
+        // uint feeInEther = uint(calculateMinRequiredWeiFee());
+
+        require(msg.value >= tweetFeeEth || accountTokenBalance >= tweetFeeToken, "TweetVoter: msg.value_TOO_LOW");
+
+        if (msg.value >= tweetFeeEth) {
+            tvTokenContract.mint(msg.sender, 5);
+        } else {
+            tvTokenContract.burn(msg.sender, 5);   
+        }
+
+        totalTweetsCount = totalTweetsCount.add(1);        
+       
+        uint tweetId = totalTweetsCount;
+
+        Tweet memory newTweet = Tweet(
+            tweetId,
+            _tweetUrl,
+            _feeReceiver,
+            msg.sender,
+            0,
+            block.timestamp
+        );
+
+        tweetIdToTweet[tweetId] = newTweet;
+        accountToTweetIds[msg.sender].push(tweetId);
+        accountTweetsAmount[msg.sender] = accountTweetsAmount[msg.sender].add(1);
+        
+        tvTokenContract.mint(msg.sender, 5);
+
+        emit SubmitTweet(msg.sender, tweetId, block.timestamp);
+    }
+
+    function likeTweet(uint256 tweetId) external payable {
+        uint accountTokenBalance = tvTokenContract.balanceOf(msg.sender);
+        Tweet storage likedTweet = tweetIdToTweet[tweetId];
+
+        require(msg.value > likeFeeEth || accountTokenBalance >= likeFeeToken, "TweetVoter: likeFee_TOO_LOW");
+
+
+        if (msg.value > likeFeeEth) {
+            (bool sent, ) = likedTweet.feeReceiver.call{value: msg.value * ((100 - platformFee) / 100)}("");
+            require(sent, "Failed to send ETH");
+            tvTokenContract.mint(msg.sender, 1);
+        } else {
+            tvTokenContract.burn(msg.sender, 1);
+        }
+
+        likedTweet.likes = likedTweet.likes.add(1);
+        
+       
+        emit LikedTweet(tweetId, likedTweet.likes, block.timestamp);
+    }
+
+    function setTweetFeeToken(uint _tweetFeeToken) external onlyOwner {
+        tweetFeeToken = _tweetFeeToken;
+    }
+    function setTweetFeeEth(uint _tweetFeeEth) external onlyOwner {
+        tweetFeeEth = _tweetFeeEth;
+    }
+    function setLikeFeeToken(uint _likeFeeToken) external onlyOwner {
+        likeFeeToken = _likeFeeToken;
+    }
+    function setLikeFeeEth(uint _likeFeeEth) external onlyOwner {
+        likeFeeEth = _likeFeeEth;
+    }
+
+    function getAllTweets() external view returns(Tweet[] memory) {
+        Tweet[] memory allTweets = new Tweet[](totalTweetsCount);
+        for (uint256 i = 1; i <= totalTweetsCount; i++) {
+            allTweets[i] = tweetIdToTweet[i];
+        }
+        return allTweets;
+    }
+
+    function getTweetById(uint tweetId) external view returns(Tweet memory) {
+        return tweetIdToTweet[tweetId];
+    }
+
+    function getTotalTweets() external view returns (uint) {
+        return totalTweetsCount;
+    }
+
+    function getAccountTweetsAmount(address account) external view returns (uint) {
+        return accountToTweetIds[account].length;
+    }
+
+    function getAllTweetsByAccount(address account) external view returns(uint[] memory) {
+        return  accountToTweetIds[account];
+    }
+
+}
+    // interface AggregatorV3Interface {
 //   function decimals() external view returns (uint8);
 
 //   function description() external view returns (string memory);
@@ -67,172 +231,76 @@ contract TVToken is ERC20, AccessControl {
 //       uint80 answeredInRound
 //     );
 // }
-interface ITVToken {
-    function mint(address to, uint256 amount) external;
-    function burn(address from, uint256 amount) external;
-    function balanceOf(address) external returns(uint) ;
-}
-
-contract TweetVoter is Ownable, ITVToken, AggregatorV3Interface {
-    event SubmitTweet(address indexed user, uint256 indexed tweetId, uint256 timestamp);
-    event LikedTweet(uint256 tweetId, uint256 likes, uint256 timestamp);
-    event Log(string func, address sender, uint value, bytes data);
-
-    using SafeMath for uint256;
-
-    ITVToken tvTokenContract;
-    AggregatorV3Interface internal priceFeed;
-
-    uint256 totalTweetsCount = 0;
-    uint256 platformFee = 5; // %
-    // uint256 tweetFee = 0.001 ether;
-    uint tweetFeeDollar = 2; // 2 dollar
-    
-    struct Tweet {
-        uint256 tweetId;
-        string tweetUrl;
-        uint256 likeFee; // dollar
-        address feeReceiverAccount;
-        
-        address tweetOwner;
-        uint256 likes;
-        uint256 timestamp;
-    }
-    
-    mapping(address => uint) public accountTweetsAmount;
-
-    mapping(address => uint[]) public accountToTweetIds;
-    mapping(uint => Tweet) public tweetIdToTweet;
-
-    constructor(address _tvTokenContract) {
-        console.log("Constructor: Tweet Voter deployed");
-        tvTokenContract = ITVToken(_tvTokenContract);
-
-        // rinkeby eth/usd
-        priceFeed = AggregatorV3Interface(0x8A753747A1Fa494EC906cE90E9f37563A8AF630e);
-	
-    }
-
-    fallback() external payable {
-        emit Log("fallback", msg.sender, msg.value, msg.data);
-    }
-
-    /// @notice Receives Ether if no tx data was added
-    receive() external payable {
-        emit Log("receive", msg.sender, msg.value, "");
-    }
-
-    function withdraw(address payable to, uint amount) external onlyOwner {
-        (bool sent, ) = to.call{value: amount}("");
-        require(sent, "Failed to send ETH");
-    }
 
     // rinkeby eth/usd
-    function getLatestPrice() internal view returns (int) {
-        (
-            /*uint80 roundID*/,
-            int price,
-            /*uint startedAt*/,
-            /*uint timeStamp*/,
-            /*uint80 answeredInRound*/
-        ) = priceFeed.latestRoundData();
-        return price;
-    }
+    // function getLatestPrice() public view returns (int) {
+    //     (
+    //         /*uint80 roundID*/,
+    //         int price,
+    //         /*uint startedAt*/,
+    //         /*uint timeStamp*/,
+    //         /*uint80 answeredInRound*/
+    //     ) = priceFeed.latestRoundData();
+    //     return price;
+    // }
 
-    function calculateMinRequiredEtherFee() internal view returns(uint){
-        uint ethPrice = uint256(getLatestPrice());
-        return tweetFeeDollar / ethPrice;
-    }
-
-    function submitTweet(
-        string memory _tweetUrl,
-        uint256 _likeFee,
-        address _feeReceiverAccount
-    ) external payable {
-        require(bytes(_tweetUrl).length > 0,"TweetVoter: tweetUrl_INVALID");
-        require(_likeFee > 0,"TweetVoter: likeFee_INVALID");
-        require(_feeReceiverAccount != address(0),"TweetVoter: feeReceiver_ZERO_ADDR");
-
-        uint accountTokenBalance = tvTokenContract.balanceOf(msg.sender);
-        uint feeInEther = calculateMinRequiredEtherFee();
-
-        require(msg.value >= feeInEther || accountTokenBalance > 4, "TweetVoter: msg.value_TOO_LOW");
-
-        if (msg.value >= feeInEther) {
-            tvTokenContract.mint(msg.sender, 5);
-        } else {
-            tvTokenContract.burn(msg.sender, 5);   
-        }
-
-        totalTweetsCount = totalTweetsCount.add(1);        
-       
-        uint tweetId = totalTweetsCount;
-
-        Tweet memory newTweet = Tweet(
-            tweetId,
-            _tweetUrl,
-            _likeFee,
-            _feeReceiverAccount,
-            msg.sender,
-            0,
-            block.timestamp
-        );
-
-        tweetIdToTweet[tweetId] = newTweet;
-        accountToTweetIds[msg.sender].push(tweetId);
-        accountTweetsAmount[msg.sender] = accountTweetsAmount[msg.sender].add(1);
-        
-        tvTokenContract.mint(msg.sender, 5);
-
-        emit SubmitTweet(msg.sender, tweetId, block.timestamp);
-    }
-
-    function likeTweet(uint256 tweetId) external payable {
-        uint accountTokenBalance = tvTokenContract.balanceOf(msg.sender);
-        Tweet storage likedTweet = tweetIdToTweet[tweetId];
-
-        require(msg.value > likedTweet.likeFee || accountTokenBalance > 0, "TweetVoter: likeFee_TOO_LOW");
-
-
-        if (msg.value > likedTweet.likeFee) {
-            (bool sent, ) = likedTweet.feeReceiverAccount.call{value: msg.value * ((100 - platformFee) / 100)}("");
-            require(sent, "Failed to send ETH");
-            tvTokenContract.mint(msg.sender, 1);
-        } else {
-            tvTokenContract.burn(msg.sender, 1);
-        }
-
-        likedTweet.likes = likedTweet.likes.add(1);
-        
-       
-        emit LikedTweet(tweetId, likedTweet.likes, block.timestamp);
-    }
-
-    function getAllTweets() external view returns(Tweet[] memory) {
-        Tweet[] memory allTweets = new Tweet[](totalTweetsCount);
-        for (uint256 i = 1; i <= totalTweetsCount; i++) {
-            allTweets[i] = tweetIdToTweet[i];
-        }
-        return allTweets;
-    }
-
-    function getTweetById(uint tweetId) external view returns(Tweet memory) {
-        return tweetIdToTweet[tweetId];
-    }
-
-    function getTotalTweets() external view returns (uint) {
-        return totalTweetsCount;
-    }
-
-    function getAccountTweetsAmount(address account) external view returns (uint) {
-        return accountToTweetIds[account].length;
-    }
-
-    function getAllTweetsByAccount(address account) external view returns(uint[] memory) {
-        return  accountToTweetIds[account];
-    }
+    // function calculateMinRequiredWeiFee() public view returns(int){
+    //     int ethPrice = getLatestPrice();
+    //     // console.log("ethPrice", ethPrice);
+    //     return 10**18 * 10**8  / ethPrice * 2 ;
+    // }
     
    
+//    function owner() public view override returns (address)  {
+//         return _owner;
+//     }
 
-}
+   
+
+//     function renounceOwnership() public override onlyOwner {
+//         _transferOwnership(address(0));
+//     }
+
+//     function transferOwnership(address newOwner) public override onlyOwner {
+//         require(newOwner != address(0), "Ownable: new owner is the zero address");
+//         _transferOwnership(newOwner);
+//     }
+
+//     function _transferOwnership(address newOwner) internal override {
+//         address oldOwner = _owner;
+//         _owner = newOwner;
+//         emit OwnershipTransferred(oldOwner, newOwner);
+//     }
+
+
+//   function decimals() external view virtual returns (uint8);
+//   function description() external view override returns (string memory);
+
+//   function version() external view override returns (uint256);
+
+//   function getRoundData(uint80 _roundId)
+//     external
+//     view override
+//     returns (
+//       uint80 roundId,
+//       int256 answer,
+//       uint256 startedAt,
+//       uint256 updatedAt,
+//       uint80 answeredInRound
+//     );
+
+//     function latestRoundData()
+//     external
+//     view override
+//     returns (
+//       uint80 roundId,
+//       int256 answer,
+//       uint256 startedAt,
+//       uint256 updatedAt,
+//       uint80 answeredInRound
+//     );
+
+
+   // rinkeby eth/usd 
+        // priceFeed = AggregatorV3Interface(0x8A753747A1Fa494EC906cE90E9f37563A8AF630e);
 
--- scripts/run.js
+++ scripts/run.js
@@ -1,4 +1,9 @@
+const keccak256 = require('keccak256')
+
 const main = async () => {
+	const [owner, randomPerson_2, randomPerson_3, randomPerson_4] =
+		await hre.ethers.getSigners()
+
 	const TVTokenContractFactory = await hre.ethers.getContractFactory('TVToken')
 	const TVTokenContract = await TVTokenContractFactory.deploy()
 	await TVTokenContract.deployed()
@@ -7,9 +12,73 @@ const main = async () => {
 	const tweetVoterContractFactory = await hre.ethers.getContractFactory(
 		'TweetVoter'
 	)
-	const tweetVoterContract = await tweetVoterContractFactory.deploy()
-	await tweetVoterContract.deployed(TVTokenContract.address)
-	console.log('Contract deployed to:', tweetVoterContract.address)
+	const tweetVoterContract = await tweetVoterContractFactory.deploy(
+		TVTokenContract.address
+	)
+	await tweetVoterContract.deployed()
+	console.log('tweetvoter Contract deployed to:', tweetVoterContract.address)
+
+	let grantMINTRole = await TVTokenContract.grantRole(
+		keccak256('MINTER_ROLE'),
+		tweetVoterContract.address
+	)
+	await grantMINTRole
+	console.log('grantMINTRole Done: ', tweetVoterContract.address)
+
+	let grantBURNRole = await TVTokenContract.grantRole(
+		keccak256('BURNER_ROLE'),
+		tweetVoterContract.address
+	)
+	await grantBURNRole
+	console.log('grantBURNRole Done: ', tweetVoterContract.address)
+
+	let tweetSubmitTx_1 = await tweetVoterContract
+		.connect(randomPerson_2)
+		.submitTweet(
+			'https://twitter.com/Marty_cFly/status/1380145053830631426',
+			randomPerson_3.address,
+			{ value: ethers.utils.parseEther('0.005') }
+		)
+	await tweetSubmitTx_1
+	console.log('tweetSubmitTx_1 Confirmed')
+
+	let tweetSubmitTx_2 = await tweetVoterContract
+		.connect(randomPerson_2)
+		.submitTweet(
+			'https://twitter.com/Marty_cFly/status/1516248898666434562',
+			randomPerson_3.address,
+			{ value: ethers.utils.parseEther('0.05') }
+		)
+	await tweetSubmitTx_2
+	console.log('tweetSubmitTx_2 Confirmed')
+
+	let tweetSubmitTx_3 = await tweetVoterContract
+		.connect(randomPerson_2)
+		.submitTweet(
+			'https://twitter.com/Marty_cFly/status/1516127328665063426',
+			randomPerson_3.address
+		)
+	await tweetSubmitTx_3
+	console.log('tweetSubmitTx_3 Confirmed')
+
+	let tweetSubmitTx_4 = await tweetVoterContract
+		.connect(randomPerson_3)
+		.submitTweet(
+			'https://twitter.com/Marty_cFly/status/1516193904936341505',
+			randomPerson_4.address,
+			{ value: ethers.utils.parseEther('0.07') }
+		)
+	await tweetSubmitTx_4
+	console.log('tweetSubmitTx_4 Confirmed')
+
+	let failedTweetSubmitTx_5 = await tweetVoterContract
+		.connect(randomPerson_4)
+		.submitTweet(
+			'https://twitter.com/Marty_cFly/status/1516193904936341505',
+			randomPerson_4.address
+		)
+	await failedTweetSubmitTx_5
+	console.log('failedTweetSubmitTx_5 Confirmed:', failedTweetSubmitTx_5)
 }
 
 const runMain = async () => {
